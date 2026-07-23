0d31128ee07a064c1ee28164b93fe7b448c9ffa7
Vlad <vlad.pokroev.99@gmail.com>
2026-07-23 12:41:45 +0300

    Добавил CORS в main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 import httpx
 from pydantic import BaseModel
 import time
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 bets = []
 
 @app.get("/")
